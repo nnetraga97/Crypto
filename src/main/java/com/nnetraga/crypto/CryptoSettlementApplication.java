6444c618555d70afd6bc23ef4fc8b875a99eb5fa
nnetraga97 <nikhil_netra@hotmail.com>
2026-07-08 19:49:28 -0700

    Add Spring Boot application scaffold

---
+++ src/main/java/com/nnetraga/crypto/CryptoSettlementApplication.java
@@ -0,0 +1,11 @@
+package com.nnetraga.crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoSettlementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoSettlementApplication.class, args);
+    }
+}
